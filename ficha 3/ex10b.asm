251b8c406b8ddfa9662d9b238a81ba8356986139
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:11:48 +0100

    Update ex10b.asm

--- ficha 3/ex10b.asm
+++ ficha 3/ex10b.asm
@@ -7,28 +7,27 @@ msg BYTE "ELEMENTOS COM VALOR ABSOLUTO INFERIOR A 0FFH = %d", 13, 10, 0
 
 .code
 main PROC C
-		xor ecx, ecx ; contador a 0
-		mov DX, vecSize
-		mov edi, offset seq
+	xor ecx, ecx ; contador a 0
+	mov DX, vecSize
+	mov edi, offset seq
 CICLO:	cmp DX, 0
-		je FIM
-		mov ebx, [edi]
-		cmp ebx, 0
-		jg POSITIVO
-		neg ebx	
+	je FIM
+	mov ebx, [edi]
+	cmp ebx, 0
+	jg POSITIVO
+	neg ebx	
 POSITIVO:
-		cmp ebx, 0FFH
-		jb COLOCAZERO
-		add edi, 4
-		dec DX
-		jmp CICLO
+	cmp ebx, 0FFH
+	jb COLOCAZERO
+	add edi, 4
+	dec DX
+	jmp CICLO
 COLOCAZERO:
-		mov DWORD PTR [edi], 0
-		add edi, 4
-		dec DX
-		jmp CICLO
+	mov DWORD PTR [edi], 0
+	add edi, 4
+	dec DX
+	jmp CICLO
 FIM:	;invoke_getch
-		invoke ExitProcess, 0
-
+	invoke ExitProcess, 0
 main ENDP
 end
