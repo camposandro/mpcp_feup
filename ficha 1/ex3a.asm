dae7361260bd59f5968b90be4824b0a54da4d302
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:09:17 +0100

    Update ex3a.asm

--- ficha 1/ex3a.asm
+++ ficha 1/ex3a.asm
@@ -6,18 +6,16 @@ result BYTE "SUM = %d", 13, 10, 0
 
 .code
 main PROC C
-	   mov ecx, LENGTHOF seq
-	   mov edi, offset seq
-	   xor eax, eax
-CICLO:     jecxz FIM
-	   add eax, [edi]
-	   add edi, 4
-	   dec ecx
-	   jmp CICLO
-FIM:       invoke printf, offset result, eax
-	   ;invoke_getch
-	   invoke ExitProcess, 0
+	mov ecx, LENGTHOF seq
+	mov edi, offset seq
+	xor eax, eax
+CICLO:  jecxz FIM
+	add eax, [edi]
+	add edi, 4
+	dec ecx
+	jmp CICLO
+FIM:    invoke printf, offset result, eax
+	;invoke_getch
+	invoke ExitProcess, 0
 main ENDP
-
 End
-
