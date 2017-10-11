2b271dd66b172917f528818f0c777e8d948e2ddc
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:06:52 +0100

    Update ex3a.asm

--- ficha 1/ex3a.asm
+++ ficha 1/ex3a.asm
@@ -9,12 +9,12 @@ main PROC C
 	   mov ecx, LENGTHOF seq
 	   mov edi, offset seq
 	   xor eax, eax
-CICLO: jecxz FIM
+CICLO:     jecxz FIM
 	   add eax, [edi]
 	   add edi, 4
 	   dec ecx
 	   jmp CICLO
-FIM:   invoke printf, offset result, eax
+FIM:       invoke printf, offset result, eax
 	   ;invoke_getch
 	   invoke ExitProcess, 0
 main ENDP
