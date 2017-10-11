45c661354b1290247cbb060a048bf7749dd46724
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:14:18 +0100

    Update ex2.asm

--- ficha 3/ex2.asm
+++ ficha 3/ex2.asm
@@ -7,19 +7,18 @@ msg2 BYTE "NO BITS AT 1", 13, 10, 0
 
 .code
 main PROC C
-		mov edi, offset value
-		mov eax, [edi]
-		mov ecx, 32
+	mov edi, offset value
+	mov eax, [edi]
+	mov ecx, 32
 CICLO:	jecxz ZERO
-		shl eax, 1
-		jc FOUND
-		dec ecx
-		jmp CICLO   
+	shl eax, 1
+	jc FOUND
+	dec ecx
+	jmp CICLO   
 ZERO:	invoke printf, offset msg2, 0
-		jmp FIM
+	jmp FIM
 FOUND:  sub ecx, 1
-		invoke printf, offset msg, ecx 
+	invoke printf, offset msg, ecx 
 FIM:	invoke	ExitProcess, 0
-
 main ENDP
 End
