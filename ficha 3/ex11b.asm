8e11a0e0ba58cf20ad589f97b8aa8fe2fb3cf32e
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:13:38 +0100

    Update ex11b.asm

--- ficha 3/ex11b.asm
+++ ficha 3/ex11b.asm
@@ -7,13 +7,11 @@ overflow BYTE "Ocorreu overflow, impossivel calcular media" ,10,13,0
 
 .code
 main PROC C
-
-mov ecx, lengthof seq
-mov esi, offset seq
-xor eax, eax
-
-next: 
-	mov edx, [esi]
+	mov ecx, lengthof seq
+	mov esi, offset seq
+	xor eax, eax
+	
+next: 	mov edx, [esi]
 	add eax, edx
 	jo erro
 	add esi, 4
@@ -25,17 +23,14 @@ next:
 	cmp edx, ecx
 	jng fim
 	inc eax
-
-fim: 
-	invoke printf, offset resultado, eax
+	
+fim: 	invoke printf, offset resultado, eax
 	jmp terminar
 
-erro: 
-	invoke printf, offset overflow
+erro: 	invoke printf, offset overflow
 
 terminar: 
 	invoke _getch
 	invoke ExitProcess, 0
-
 main ENDP
 end
