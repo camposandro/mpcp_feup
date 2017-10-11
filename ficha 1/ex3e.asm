183299f61b816d5cce48d7d1c757aa726e73d16c
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:07:53 +0100

    Update ex3e.asm

--- ficha 1/ex3e.asm
+++ ficha 1/ex3e.asm
@@ -5,19 +5,18 @@ msg BYTE "AMOUNT OF PAIR NUMBERS IN MEMORY BETWEEN POSITIONS 10F000H AND 11B003H
 
 .code
 main PROC C
-		xor eax, eax
-		mov edi, 10F000H ;; pesquisa em memoria entre
+	xor eax, eax
+	mov edi, 10F000H ;; pesquisa em memoria entre
 CICLO:	cmp edi, 11B003H ;; as posicoes 10F000H e 11B003H 
-		ja FIM 
-		mov ebx, [edi]
-		add edi, 4
-		shr ebx, 1
-		jc CICLO ;; se bit mais significativo 1 entao impar
-		inc eax ;; se par incrementa-se eax
-		jmp CICLO
+	ja FIM 
+	mov ebx, [edi]
+	add edi, 4
+	shr ebx, 1
+	jc CICLO ;; se bit mais significativo 1 entao impar
+	inc eax ;; se par incrementa-se eax
+	jmp CICLO
 FIM:	invoke printf, offset msg, eax
-		;invoke_getch
-		invoke ExitProcess, 0
+	;invoke_getch
+	invoke ExitProcess, 0
 main ENDP
-
 End
