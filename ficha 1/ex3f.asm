addbf71c21ca8302f5468f8551d7b53b8446008a
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:08:22 +0100

    Update ex3f.asm

--- ficha 1/ex3f.asm
+++ ficha 1/ex3f.asm
@@ -5,20 +5,19 @@ msg BYTE "NUMBER OF UPPERCASES IN MEMORY FROM POSITION AAF000H TO BB003H = %d", 
 
 .code
 main PROC C
-		xor eax, eax ;; inicializacao do contador
-		mov edi, 0AAF000H ;; posicao de memoria inicial
+	xor eax, eax ;; inicializacao do contador
+	mov edi, 0AAF000H ;; posicao de memoria inicial
 CICLO:	cmp edi, 0BB003H ;; posicao de memoria final
-		ja FIM
-		mov ebx, [edi]
-		inc edi
-		.IF ebx >= 41H && ebx <= 5AH ;; se for letra maiuscula
-		inc eax
-		.ELSE
-		jmp CICLO 
-		.ENDIF
+	ja FIM
+	mov ebx, [edi]
+	inc edi
+	.IF ebx >= 41H && ebx <= 5AH ;; se for letra maiuscula
+	inc eax
+	.ELSE
+	jmp CICLO 
+	.ENDIF
 FIM:	invoke printf, offset msg, eax
-		;invoke_getch
-		invoke ExitProcess, 0
+	;invoke_getch
+	invoke ExitProcess, 0
 main ENDP
-
 End
