880064d2dc5ec163cf824e2dc929d539dfd335a5
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:02:06 +0100

    Update ex3.asm

--- ficha 2/ex3.asm
+++ ficha 2/ex3.asm
@@ -7,22 +7,21 @@ msg2 BYTE "SEQUENCIA CRESCENTE", 13, 10, 0
 
 .code
 main PROC C
-		mov esi, OFFSET seq ; endereco seq
-		mov ecx, LENGTHOF seq ; contador
-		dec ecx
+	mov esi, OFFSET seq ; endereco seq
+	mov ecx, LENGTHOF seq ; contador
+	dec ecx
 
 CICLO:	mov ebx, [esi]
-		mov edi, [esi + 4] ; 2o elem 
-		add esi, 4 ; incrementar para prox. iteracao
-		cmp edi, ebx
-		jbe DECR ; decrescente
-		loop CICLO
+	mov edi, [esi + 4] ; 2o elem 
+	add esi, 4 ; incrementar para prox. iteracao
+	cmp edi, ebx
+	jbe DECR ; decrescente
+	loop CICLO
 CRES:	invoke printf, offset msg2, 0
-		jmp FIM
+	jmp FIM
 DECR:	invoke printf, offset msg, 0
 
 FIM:	;invoke_getch
-		invoke ExitProcess, 0
-
+	invoke ExitProcess, 0
 main ENDP
 End
