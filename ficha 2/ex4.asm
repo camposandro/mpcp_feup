22422a667e494c32284c1f8f69a00a1bf17783f5
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:03:13 +0100

    Update ex4.asm

--- ficha 2/ex4.asm
+++ ficha 2/ex4.asm
@@ -7,42 +7,42 @@ msg BYTE "SOMA = %d", 13, 10, 0
 
 .code
 main PROC C
-			mov esi, offset seq1
-			mov edi, offset seq2
-			mov BX, LENGTHOF seq1
-			mov DX, LENGTHOF seq2
-			xor AX, AX ; somador
+	mov esi, offset seq1
+	mov edi, offset seq2
+	mov BX, LENGTHOF seq1
+	mov DX, LENGTHOF seq2
+	xor AX, AX ; somador
 
-CICLO:		add AX, [esi]
-			jo OVERFLOW1
-			add AX, [edi]
-			jo OVERFLOW2
-			add esi, 2
-			add edi, 2
-			loop CICLO
-			jmp FIM
+CICLO:	add AX, [esi]
+	jo OVERFLOW1
+	add AX, [edi]
+	jo OVERFLOW2
+	add esi, 2
+	add edi, 2
+	loop CICLO
+	jmp FIM
 
-			;; havendo overflow, se ultimo valor de [edi]
-			;; e se for positivo entao apresentar
-			;; o valor maximo positivo (0FFFH)
-			;; caso contrario apresentar (F000H)
+	;; havendo overflow, se ultimo valor de [edi]
+	;; e se for positivo entao apresentar
+	;; o valor maximo positivo (0FFFH)
+	;; caso contrario apresentar (F000H)
 
 OVERFLOW1:	mov AX, [esi]
-			cmp AX, 0
-			js NEGATIVO1
-			mov AX, 0FFFH
-			jmp FIM
+		cmp AX, 0
+		js NEGATIVO1
+		mov AX, 0FFFH
+		jmp FIM
 NEGATIVO1:	mov AX, 0F000H
-			jmp FIM
+		jmp FIM
 OVERFLOW2:	mov AX, [edi]
-			cmp AX, 0
-			js NEGATIVO2
-			mov AX, 0FFFH
-			jmp FIM
+		cmp AX, 0
+		js NEGATIVO2
+		mov AX, 0FFFH
+		jmp FIM
 NEGATIVO2:	mov AX, 0F000H
 
 FIM:		invoke printf, offset msg, AX
-			;invoke_getch
-			invoke ExitProcess, 0
+		;invoke_getch
+		invoke ExitProcess, 0
 main ENDP
 end
