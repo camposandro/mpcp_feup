67bd2df3c7d8d34cd28a20c6c7df186a9ae21882
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:05:03 +0100

    Update ex6.asm

--- ficha 2/ex6.asm
+++ ficha 2/ex6.asm
@@ -1,24 +1,21 @@
 include mpcp.inc
 
-;; declaracoes de dados (variaveis globais)
 .data
 seq SBYTE 1,-2,3,4,5,-6
 msg SBYTE "Somador: %d", 13, 10, 0
 
-;; seccao de codigo principal 
 .code
 main PROC C
 	;; escrever codigo (parte principal)
-		mov edi, offset seq
-		mov ecx, LENGTHOF seq
-		xor eax, eax
+	mov edi, offset seq
+	mov ecx, LENGTHOF seq
+	xor eax, eax
 ciclo:	add eax, [edi]
-		inc edi
-		loop ciclo
+	inc edi
+	loop ciclo
 fim:	invoke printf, offset msg, eax
-		;invoke_getch
-	;; Terminar o programa: Não omitir!
-		invoke ExitProcess, 0
-
+	;invoke_getch
+	;; Terminar o programa: NÃ£o omitir!
+	invoke ExitProcess, 0
 main ENDP
 End
