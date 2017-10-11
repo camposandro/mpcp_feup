8a66c75085a5bdae4b1f69731d3cdd425b252868
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:04:31 +0100

    Update ex5a.asm

--- ficha 2/ex5a.asm
+++ ficha 2/ex5a.asm
@@ -8,26 +8,22 @@ seq DWORD 5555 457821453 12145
 .code
 
 main PROC C
-	;; escrever codigo (parte principal)
 	   xor eax, eax
 	   xor DI, DI
 	   mov edi, offset seq
 	   mov ecx, LENGTHOF seq
 ; testa se valor e representavel
-teste: mov ebx, [edi]
+teste: 	   mov ebx, [edi]
 	   shr ebx, 16 
 	   cmp ebx, FFFFh
 	   jz tratarvalor
 	   cmp ebx, 0
 	   jz tratarvalor
-tratarvalor: mov [edi]
-		jmp ciclo
+tratarvalor: 
+	   mov [edi]
+	   jmp ciclo
 
-	;; Terminar o programa: Não omitir!
-		invoke	ExitProcess, 0
-
+	;; Terminar o programa: NÃ£o omitir!
+	    invoke ExitProcess, 0
 main ENDP
-;; -----------------------------
-;; codigo de outras rotinas
-
 End
