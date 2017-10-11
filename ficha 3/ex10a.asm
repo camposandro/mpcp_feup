2626ed9e8113d7cfedc0e13965c25eed729953d3
Sandro Campos <up201605947@fe.up.pt>
2017-10-11 21:11:15 +0100

    Update ex10a.asm

--- ficha 3/ex10a.asm
+++ ficha 3/ex10a.asm
@@ -9,26 +9,25 @@ msg BYTE "ELEMENTOS IGUAIS EM VALOR ABSOLUTO AO CONTEUDO DE EAX = %d", 13, 10, 0
 
 .code
 main PROC C
-		xor ecx, ecx ; contador a 0
-		mov DX, vecSize
-		mov edi, offset seq
-		mov eax, 1
+	xor ecx, ecx ; contador a 0
+	mov DX, vecSize
+	mov edi, offset seq
+	mov eax, 1
 CICLO:	cmp DX, 0
-		je FIM
-		mov ebx, [edi]
-		add edi, 4
-		dec DX
-		cmp ebx, eax
-		je FOUND ;valor igual positivo
-		neg ebx
-		cmp ebx, eax
-		je FOUND ;se valor for igual em negativo
-		jmp CICLO
+	je FIM
+	mov ebx, [edi]
+	add edi, 4
+	dec DX
+	cmp ebx, eax
+	je FOUND ;valor igual positivo
+	neg ebx
+	cmp ebx, eax
+	je FOUND ;se valor for igual em negativo
+	jmp CICLO
 FOUND: 	inc ecx
-		jmp CICLO
+	jmp CICLO
 FIM: 	invoke printf, offset msg, ecx
-		;invoke_getch
-		invoke ExitProcess, 0
-
+	;invoke_getch
+	invoke ExitProcess, 0
 main ENDP
 end
